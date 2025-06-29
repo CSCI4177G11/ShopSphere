ebebac68ba8569ec257784138e12a024a7027122
whyte <sr869233@dal.ca>
2025-06-29 10:57:04 -0300

    Auth service folders added

---
+++ auth-service/src/routes/authRoutes.js
@@ -0,0 +1,7 @@
+import {Router} from 'express';
+const controller = reauire('../controllers/authController');
+
+const route = Router();
+
+route.post('/login', controller.login);
+Router.post('/registerUser', controller.registerUser);
