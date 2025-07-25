c7b5463e0cc6a5cd6cc92b94ea08d61208d85ddc
abalsalmi <aman.alsalmi3112@gmail.com>
2025-07-25 18:13:39 -0300

    Increase limit

--- user-service/src/index.js
+++ user-service/src/index.js
@@ -16,7 +16,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.use(cors({ origin: CORS_ORIGIN, credentials: true }));
 app.use(morgan(NODE_ENV === 'production' ? 'tiny' : 'dev'));
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/user/health', (req, res) => {
